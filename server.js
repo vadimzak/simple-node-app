9c7691850a6bd1b3d3c154bb9e22bba9029708ac
vz <vadimzak@gmail.com>
2018-12-06 16:07:46 +0200

    bugfix

--- server.js
+++ server.js
@@ -7,20 +7,16 @@ const secret = process.env.SECRET || 'secret'
 const endCompany = process.env.END_COMPANY || 'guest'
 
 // Constants
-const PORT = 3000;
+const PORT = 3001;
 
 // App
 const app = express();
 app.get('/', (req, res) => {
-  try {
-    const token = req.query.token
+  const token = req.query.token
 
-    const jwtObj = jwt.verify(token, secret, { algorithm: 'HS256' })
+  const jwtObj = jwt.verify(token, secret, { algorithm: 'HS256' })
 
-    res.send(`Hello ${endCompany}!\n\n${JSON.parse(jwtObj, null, 4)}`);
-  } catch (err) {
-    res.send(`Error!\n\n${err.toString()}`);
-  }
+  res.send(`Hello ${endCompany}!\n\n${JSON.stringify(jwtObj, null, 4)}`);
 });
 
 app.listen(PORT);
