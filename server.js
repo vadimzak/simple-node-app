c260522f805548810101260f9fe2decba69d0d2a
vz <vadimzak@gmail.com>
2018-12-06 15:52:38 +0200

    jwt added

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 'use strict';
 
 const express = require('express');
+const jwt = require('jsonwebtoken');
+
+const secret = process.env.SECRET || 'secret'
+const endCompany = process.env.END_COMPANY || 'guest'
 
 // Constants
 const PORT = 3000;
 
 // App
 const app = express();
-app.get('/', function (req, res) {
-  res.send(`Hello ${process.env.USERNAME || 'guest'}!`);
+app.get('/', async (req, res) => {
+  const token = req.query.token
+
+  const jwtObj = jwt.verify(token, secret, { algorithm: 'HS256' })
+
+  res.send(`Hello ${endCompany}!\n\n${JSON.parse(jwtObj, null, 4)}`);
 });
 
 app.listen(PORT);
