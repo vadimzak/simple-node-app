b8fbe9dd94c0ef3c710c2586e4211930df4f6b7c
vz <vadimzak@gmail.com>
2018-12-13 21:09:49 +0200

    changes

--- server.js
+++ server.js
@@ -4,24 +4,24 @@ const express = require('express');
 // const jwt = require('jsonwebtoken');
 
 // const jwtSecret = process.env.JWT_SECRET
-const endCompany = process.env.END_COMPANY || 'guest'
 
 
 // Constants
 const PORT = 3000;
 
-const html = `<body style="background-color: ${process.env.BACKGROUND_COLOR}; color: ${process.env.TEXT_COLOR}">
-Hello ${endCompany},<br>
-<ol>
-${Array(Number(process.env.BULLETS)).keys().map(i) => `<li>${process.env.MOTO}</li>`}
-</ol>
-</body>
-`
-
 // App
 const app = express();
 app.get('/', (req, res) => {
   try {
+    const html = `
+      <body style="background-color: ${process.env.BACKGROUND_COLOR}; color: ${process.env.TEXT_COLOR}">
+        Hello ${process.env.END_COMPANY || 'guest'},<br>
+        <ol>
+          ${Array(Number(process.env.BULLETS)).keys().map(i) => `<li>${process.env.MOTO}</li>`
+        }
+        </ol >
+      </body >
+    `
     // const token = req.query.token
     // const jwtObj = jwt.verify(token, jwtSecret, { algorithm: 'HS256' })
     res.send(html);
