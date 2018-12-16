9271fa291a9ea2d9aa8f8c26cf98f35e8129e8d1
vz <vadimzak@gmail.com>
2018-12-16 11:46:53 +0200

    auto-commit

--- server.js
+++ server.js
@@ -19,8 +19,17 @@ const databaseConnectionOpts = process.env.DATABASE_CONNECTION_OPTIONS.trim();
 const dbUrl: `mongodb://${databaseUser}:${databasePassword}@${databaseHost}:${databasePort}/${databaseName}?${databaseConnectionOpts}`
 const dbName = 'simple-node-app';
 
-function updateHitCount() {
+async function updateHitCount() {
+  const client = await MongoClient.connect(url)
+  try {
+    const db = client.db(dbName);
+    const hitsCollection = db.collection('hits');
 
+    await hitsCollection.insert([{ time: Date.now() }])
+    return await hitsCollection.count()
+  } finally {
+    client.close();
+  }
 }
 
 
