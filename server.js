21c76629017245e1b16cb96ef966937b1fdcab5e
vz <vadimzak@gmail.com>
2018-12-16 11:44:39 +0200

    auto-commit

--- server.js
+++ server.js
@@ -1,29 +1,50 @@
 'use strict';
 
 const express = require('express');
-// const jwt = require('jsonwebtoken');
+const MongoClient = require('mongodb').MongoClient;
 
-// const jwtSecret = process.env.JWT_SECRET
+var database = require('./config/database');                      // load the database config
 
 
 // Constants
 const PORT = 3000;
 
+const databaseHost = process.env.DATABASE_HOST.trim();
+const databasePort = process.env.DATABASE_PORT.trim();
+const databaseUser = process.env.DATABASE_USER.trim();
+const databasePassword = process.env.DATABASE_PASSWORD.trim();
+const databaseName = process.env.DATABASE_NAME.trim();
+const databaseConnectionOpts = process.env.DATABASE_CONNECTION_OPTIONS.trim();
+
+const dbUrl: `mongodb://${databaseUser}:${databasePassword}@${databaseHost}:${databasePort}/${databaseName}?${databaseConnectionOpts}`
+const dbName = 'simple-node-app';
 
 // App
 const app = express();
 app.get('/', (req, res) => {
   try {
+
+    let count = null
+
+    const client = await MongoClient.connect(url)
+    try {
+      const db = client.db(dbName);
+      const hitsCollection = db.collection('hits');
+      // Insert some documents
+      await hitsCollection.insert([{ time: Date.now() }]
+      count = await hitsCollection.count()
+    } finally {
+      client.close();    
+    }
+
     const html = `
       <body style="background-color: ${process.env.BACKGROUND_COLOR}; color: ${process.env.TEXT_COLOR}">
-        Hello ${process.env.END_COMPANY || 'guest'},<br>
+        Hello ${process.env.END_COMPANY || 'guest'}, (hitcount: ${count})<br>
         <ol>
-          ${[...Array(Number(process.env.BULLETS)).keys()].map(i => `<li>${process.env.MOTO}</li>`)}
+          ${[...Array(Number(process.env.BULLETS)).keys()].map(i => `<li>${process.env.MOTO}</li>`).join('')}
         </ol >
       </body >
     `
-    // const token = req.query.token
-    // const jwtObj = jwt.verify(token, jwtSecret, { algorithm: 'HS256' })
     res.send(html);
   } catch (err) {
     res.send(`Error!\n\n${err.toString()}`);    
