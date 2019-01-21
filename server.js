1071b0a3941cb971ebf4c675c4c1190175d63cec
Vadim Zak <vadimzak@gmail.com>
2019-01-21 12:22:41 +0200

    auto-commit

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 'use strict'
 
 const express = require('express')
+const basicAuth = require('express-basic-auth')
 
 const { updateHitCount, getLastAgentRequest, registerAgentRequest } = require('./db')
+
+function readEnv (envVarName) {
+  return process.env[envVarName] || (() => { throw new Error(`Env var '${envVarName}' is unset`)})
+}
+
+const endCompany = readEnv('END_COMPANY')
+const accountApiKey = readEnv('ACCOUNT_API_KEY')
+const subscription = readEnv('SUBSCRIPTION')
+const sla = readEnv('SLA')
+const months = readEnv('MONTHS')
 
 ;(async () => {
   try {
@@ -25,10 +36,10 @@ async function startWebServer () {
 
       const html = `
       <body>
-        Hello <strong>${process.env.END_COMPANY || 'guest'}</strong>, (hitcount: <strong>${hitCount}</strong>)<br>
-        Subscription type: <strong>${process.env.SUBSCRIPTION}</strong><br>
-        SLA: <strong>${process.env.SLA}</strong><br>
-        Months: <strong>${process.env.MONTHS}</strong><br>
+        Hello <strong>${endCompany}</strong>, (hitcount: <strong>${hitCount}</strong>)<br>
+        Subscription type: <strong>${subscription}</strong><br>
+        SLA: <strong>${sla}</strong><br>
+        Months: <strong>${months}</strong><br>
         Agent hostname: <strong>${JSON.stringify(agentHostname)}</strong><br>
       </body>
     `
@@ -48,6 +59,11 @@ async function startAgentServer () {
   const AGENT_PORT = 4000
 
   const agentApp = express()
+
+  agentApp.use(basicAuth({
+    users: { [endCompany]: accountApiKey }
+  }))
+
   agentApp.get('/', async (req, res) => {
     try {
       const hostname = req.query['agent-hostname']
